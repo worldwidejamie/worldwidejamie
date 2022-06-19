f40e5689f7deaea4fc012e09c3dcb5991d00112a
Jamie Smith <hello@worldwidejamie.com>
2022-06-19 12:17:20 -0500

    Add ITC font

---
+++ src/assets/scripts/scripts.js
@@ -0,0 +1,29 @@
+import anime from "animejs";
+
+anime({
+	targets: "svg text",
+	rotate: -20,
+	delay: 500,
+	easing: "spring",
+})
+
+function rotateText() {
+anime({
+	targets: "svg text",
+	rotate: 360,
+	duration: 2000,
+	easing: "spring"
+})
+}
+function reverseText() {
+	anime({
+		targets: "svg text",
+		rotate: -20,
+		duration: 4000,
+		easing: "spring"
+	})
+}
+const svgContainer = document.querySelector(".title-path");
+console.log(svgContainer)
+svgContainer.addEventListener("mouseover", rotateText);
+svgContainer.addEventListener("mouseout", reverseText);
