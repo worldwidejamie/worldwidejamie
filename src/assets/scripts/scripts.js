13f9407877fb94867310ed20a633fc5981d91676
Jamie Smith <hello@worldwidejamie.com>
2022-06-25 13:22:07 -0500

    Cleanup scripts.js

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -5,25 +5,24 @@ anime({
 	rotate: -20,
 	delay: 500,
 	easing: "spring",
-})
+});
 
 function rotateText() {
-anime({
-	targets: "svg text",
-	rotate: 360,
-	duration: 4000,
-	easing: "spring"
-})
+	anime({
+		targets: "svg text",
+		rotate: 360,
+		duration: 4000,
+		easing: "spring",
+	});
 }
 function reverseText() {
 	anime({
 		targets: "svg text",
 		rotate: -20,
 		duration: 4000,
-		easing: "spring"
-	})
+		easing: "spring",
+	});
 }
 const svgContainer = document.querySelector(".title-path");
-console.log(svgContainer)
 svgContainer.addEventListener("mouseover", rotateText);
 svgContainer.addEventListener("mouseout", reverseText);
