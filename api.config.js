31a3886555c2f734687586d88cdd4dbf20a5a7a9
Jamie Smith <hello@worldwidejamie.com>
2022-06-27 22:08:40 -0500

    Update the environment variables

--- api.config.js
+++ api.config.js
@@ -2,8 +2,10 @@
  * Site Api configuration
  **/
 
+const { PUBLIC_CONTENT_API_KEY } = import.meta.env;
+
 export const apiConfig = {
 	apiURL: "https://ghost.worldwidejamie.com/ghost/api/content",
-	contentApiKey: CONTENT_API_KEY,
-	AdminApiKey: ADMIN_API_KEY,
+	contentApiKey: PUBLIC_CONTENT_API_KEY,
+	adminApiKey: ADMIN_API_KEY,
 };
