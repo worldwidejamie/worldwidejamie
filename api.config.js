1d49d383e6421aba5c818297436c3d784a4d54ed
Jamie Smith <hello@worldwidejamie.com>
2022-06-27 20:35:54 -0500

    Add env variables
    Added the env variables to `project.yml` and `api.config.js` because that needs to be committed to the repo. Which is obvious now.

---
+++ api.config.js
@@ -0,0 +1,9 @@
+/**
+ * Site Api configuration
+ **/
+
+export const apiConfig = {
+	apiURL: "https://ghost.worldwidejamie.com/ghost/api/content",
+	contentApiKey: CONTENT_API_KEY,
+	AdminApiKey: ADMIN_API_KEY,
+};
