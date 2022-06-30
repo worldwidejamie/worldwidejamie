975768389641292dd72da500e2a8fbb4bab048d0
Jamie Smith <hello@worldwidejamie.com>
2022-06-29 20:57:06 -0500

    Remove references to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), react(), tailwind()],
+	integrations: [react(), tailwind()],
 	vite: {
 		resolve: {
 			alias: {
