4fc53a42df270bc5ac3776dfe642057ff25c190b
Jamie Smith <hello@worldwidejamie.com>
2022-06-29 20:55:07 -0500

    Restore vite build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from "astro/config";
-import nodejs from "@astrojs/node";
+import preact from "@astrojs/preact";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
-	adapter: nodejs(),
+	integrations: [preact(), react(), tailwind()],
+	vite: {
+		resolve: {
+			alias: {
+				$: "/src",
+				$components: "/src/components",
+			},
+		},
+	},
 });
